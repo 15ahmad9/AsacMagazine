9e146d4dc01003db7ce071dd18114b61fbe9ef08
Ahmad Ghanem <109623987+15ahmad9@users.noreply.github.com>
2023-06-21 13:48:24 +0300

    Update memes.js

--- API_Mag/memes.js
+++ API_Mag/memes.js
@@ -1,6 +1,7 @@
 const generateMemeBtn = document.querySelector(
     ".meme-generator .generate-meme-btn"
   );
+
   const memeImage = document.querySelector(".meme-generator img");
   const memeTitle = document.querySelector(".meme-generator .meme-title");
   const memeAuthor = document.querySelector(".meme-generator .meme-author");
